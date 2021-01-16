2794b506e323dcee01eb2df6b9e912faff7d8124
NIcolasBonamy <n.bonamy@live.fr>
2021-01-16 19:53:22 +0100

    Function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -3,13 +3,13 @@ const assert = require("assert");
 
 // WRITE THE ACTUAL FUNCTION HERE
 function capitalizeFirstLetters(input) {
-  const words = input.split(' ');
-  for (i=0; i<words.length; i++) {
-      words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
+    const words = input.split(' ');
+    for (i=0; i<words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
+    }
+    return words.join(' ');
   }
-  return words.join(' ');
-}
-
+  
 // Check that capitalizeFirstLetters is a function
 assert.strictEqual(typeof capitalizeFirstLetters, "function");
 
